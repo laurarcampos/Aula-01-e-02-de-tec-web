c42106d9e297d6eb4b5b4f57b0ff6d5931665c65
Laura Campos <lauracampos1411@gmail.com>
2023-03-23 21:53:06 -0300

    TESTE

--- Aula 02/Exercício 7/menu.js
+++ Aula 02/Exercício 7/menu.js
@@ -3,6 +3,7 @@ document.querySelector("#navbar").innerHTML = `
       <nav class="navbar navbar-expand-lg navbar-dark">
             <a class="navbar-brand" href="./index.html">Menu</a>     
             <a class="navbar-brand" href="https://laurarcampos.github.io/Aula-01-e-02-de-tec-web/index.html">Voltar para o menu com todas as atividades</a>
-      </nav>
+    
+            </nav>
 </header>
 `
