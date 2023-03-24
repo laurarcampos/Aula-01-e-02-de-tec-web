bde2b942e19920dd9d159a38d3bc055ac1d1c601
Laura Campos <lauracampos1411@gmail.com>
2023-03-23 21:14:09 -0300

    primeiro commit

---
+++ Aula 02/Exercício 7/menu.js
@@ -0,0 +1,8 @@
+document.querySelector("#navbar").innerHTML = `
+<header id="navbar"> 
+      <nav class="navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand" href="./index.html">Menu</a>     
+            <a class="navbar-brand" href="/index.html">Voltar para o menu com todas as atividades</a>
+      </nav>
+</header>
+`
