2a772e5d606dbd1a13fa14ebeba7f3637c364d6a
Laura Campos <lauracampos1411@gmail.com>
2023-03-23 21:59:58 -0300

    agora vai

--- Aula 02/Exercício 7/menu.js
+++ Aula 02/Exercício 7/menu.js
@@ -2,8 +2,7 @@ document.querySelector("#navbar").innerHTML = `
 <header id="navbar"> 
       <nav class="navbar navbar-expand-lg navbar-dark">
             <a class="navbar-brand" href="./index.html">Menu</a>     
-            <a class="navbar-brand" href="https://laurarcampos.github.io/Aula-01-e-02-de-tec-web/index.html">Voltar para o menu com todas as atividades</a>
-    
-            </nav>
+            <a class="navbar-brand" href="https://laurarcampos.github.io/Aula-01-e-02-de-tec-web/index.html" >Voltar para o menu com todas as atividades</a>                
+      </nav>
 </header>
 `
